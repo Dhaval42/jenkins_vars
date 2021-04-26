f6365056ae692c3d99c442d3777ba8e16fd3738e
dhavalvaghela201 <dhavalvaghela201@gmail.com>
2021-04-26 10:13:42 +0530

    first commit

---
+++ jenkins_test/vars/email.groovy
@@ -0,0 +1,21 @@
+def notifystarted(email){
+	echo "${email}- first mail"
+    emailext (
+    	 subject: "${currentBuild.currentResult}:Job ${env.JOB_NAME} ${env.BUILD_NUMBER}",
+         body: """<p>${currentBuild.currentResult}:  Job ${env.JOB_NAME} </p>
+         	   <p> BUild NUMBER: build ${env.BUILD_NUMBER}</p>
+         	   <p> Check console Output at:<a href=${env.BUILD_URL}>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>""",
+            recipientProviders: [buildUser()],
+    )
+}
+def notifystatus(email){
+	echo "${email}- second mail"
+	 emailext (
+	  attachLog: true,     
+	   subject: "STARTED :Job ${env.JOB_NAME} ${env.BUILD_NUMBER} "   , 
+	 	body: """<p>STARTED: Job ${env.JOB_NAME} </p>
+	 	<p> BUild NUMBER: build ${env.BUILD_NUMBER}</p>
+	 	<p> Check console Output at:<a href=${env.BUILD_URL}>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>""", 
+            recipientProviders: [buildUser()]
+	)
+
